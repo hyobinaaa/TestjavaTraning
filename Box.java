58da2de710b2a76b11d9face87c1a17b73b5e7ef
イ・ヒョ・ビン <113012132+hyobinaaa@users.noreply.github.com>
2023-04-27 20:45:13 +0900

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,10 @@
+public class Box {
+  int width;
+  int length;
+  int height;
+
+  @Override
+  public String toString() {
+    return "상자의 가로 , 세로 , 높이는 " + width + ", " + length + ", " + height + "입니다.";
+  }
+}
